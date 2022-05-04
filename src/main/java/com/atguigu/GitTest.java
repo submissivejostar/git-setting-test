89e24bb0478c318e8bfb43c6bb3fcd704631af58
xiexiaoxuan <xiexiaoxuan1998@gmail.com>
2022-05-04 23:50:55 +0800

    nihao

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git！");
         System.out.println("hello github！");
         System.out.println("konijiwa github！");
+        System.out.println("nihaogithub！");
     }
 }
